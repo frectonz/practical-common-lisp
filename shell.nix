516bc72be1789e58236f108e0b0f8b38624e2b17
frectonz <fraol0912@gmail.com>
2023-11-26 09:42:27 +0300

    setup swank to use with conjure

--- shell.nix
+++ shell.nix
@@ -1,5 +1,11 @@
 { pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs.buildPackages; [ clisp sbcl rlwrap ];
+  nativeBuildInputs =
+    let
+      swank = pkgs.writeShellScriptBin "swank" ''
+        ${pkgs.roswell}/bin/ros run --eval '(ql:quickload :swank)'  --eval '(swank:create-server :dont-close t)'
+      '';
+    in
+    with pkgs; [ clisp sbcl rlwrap swank ];
 }
