61392a4f68b1987596cb549ba7c8f7e8786fc8ec
frectonz <fraol0912@gmail.com>
2023-11-26 20:15:38 +0300

    feat: `combine-results` macro

--- a-test-framework/test.lisp
+++ a-test-framework/test.lisp
@@ -1,11 +1,17 @@
 (defun report-result (result form)
-  (format t "~:[FAIL~;pass~] ... ~a~%" result form))
+  (format t "~:[FAIL~;pass~] ... ~a~%" result form)
+  result)
+
+(defmacro combine-results (&body forms)
+  `(let ((result t))
+     ,@(loop for f in forms collect `(unless ,f (setf result nil)))
+     result))
 
 ; (defmacro check (form)
 ;   (append '(report-result) (list form (list 'quote form))))
 
 (defmacro check (&body forms)
-  `(progn ,@(loop for f in forms collect `(report-result ,f ',f))))
+  `(combine-results ,@(loop for f in forms collect `(report-result ,f ',f))))
 
 (defun test-+ ()
   (check
